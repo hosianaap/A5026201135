6675442796c5a534d7a720d80fcf45973c389fd8
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2021-11-18 15:07:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,11 @@ Route::post('hasilLinearSearch', "ViewController@ResultLinearSearch");
 
 // Route::get('isiannama', "ViewController@showForm") ;
 // Route:: post('greetings', "ViewController@resultGreetings") ;
+
+// route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
