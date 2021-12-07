df82c2ef6bac013fc07665f9f813b523550ec200
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2021-12-08 00:57:58 +0700

    Update tambah.blade.php

--- resources/views/absen/tambah.blade.php
+++ resources/views/absen/tambah.blade.php
@@ -1,24 +1,69 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tambah Data Absen</title>
-</head>
-<body>
+@extends('layout.bahagia')
 
-	<h2>Tambah Data Absen</h2>
+@section('side-navbar')
+<ul class="nav nav-pills nav-stacked">
+    <li><a href="/pegawai" > Pegawai </a></li>
+    <li class="active"><a href="/absen"> Absen </a></li>
+    <li> <a href="#"> Praktikum </a> </li>
+</ul>
+@endsection
 
-	<a href="/absen"> Kembali</a>
-
-	<br/>
-	<br/>
-
+@section('judulbagian', 'Tambah Data Absen')
+@section('konten')
 	<form action="/absen/store" method="post">
 		{{ csrf_field() }}
-		IDPegawai <input type="number" name="IDPegawai" required="required"> <br/>
-		Tanggal <input type="datetime-local" name="Tanggal" required="required"> <br/>
-		Status <input type="text" name="Status" maxlength="1" required="required"> <br/>
-		<input type="submit" value="Simpan Data">
+        <div class="form-group row">
+            <label class="col-sm-2" for="IDPegawai"> ID Pegawai </label>
+            <div class="col-sm-10">
+                <select name="IDPegawai" class="form-control">
+                    @foreach($pegawai as $p)
+                     <option value="{{ $p->pegawai_id }}">{{ $p->pegawai_nama }}
+                     </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="dtpickerdemo" class="col-sm-2"> Tanggal </label>
+            <div class='col-sm-10 input-group date' id='dtpickerdemo'>
+                <input type='text' class="col-sm-10 form-control" name="Tanggal" required="required" />
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            $(function() {
+                $('#dtpickerdemo').datetimepicker({
+                    format: 'YYYY-MM-DD hh:mm:ss',
+                    showTodayButton: false,
+                    locale : 'id',
+                    "defaultDate": new Date(),
+                });
+            });
+        </script>
+
+        <div class="form-group row">
+            <label class="col-sm-2" for="Status"> Status </label>
+            <div class="col-sm-10">
+                <input type="radio" id="Hadir" name="Status" value="H">
+                <label for="Hadir">Hadir</label><br>
+                <input type="radio" id="Izin" name="Status" value="I">
+                <label for="Izin">Izin</label><br>
+                <input type="radio" id="Sakit" name="Status" value="S" checked="checked">
+                <label for="Sakit">Sakit</label><br>
+                <input type="radio" id="Alpha" name="Status" value="A">
+                <label for="Alpha">Alpha</label>
+            </div>
+        </div>
+
+        <div class="d-flex text-center">
+            <button type="submit" class="btn btn-success"> Simpan Data </button>
+            <a href="/absen" class="btn btn-info"> Kembali </a>
+        </div>
+
 	</form>
 
-</body>
-</html>
+@endsection
