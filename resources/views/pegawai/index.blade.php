81140e2034176e3fe2447ac294259f07e03e9f38
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2021-12-12 19:09:15 +0700

    Update index.blade.php

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -4,33 +4,45 @@
 <ul class="nav nav-pills nav-stacked">
     <li class="active"><a href="/pegawai" > Pegawai </a></li>
     <li><a href="/absen"> Absen </a></li>
-    <li> <a href="#"> Praktikum </a> </li>
+    <li> <a href="/beras"> Beras </a> </li>
 </ul>
 @endsection
 
 @section('judulbagian', 'Data Pegawai')
 @section('konten')
+    <div class="row">
+        <div class="col-sm-6">
+            <a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Data</a>
+        </div>
+
+        <form action="/pegawai/cari" method="GET" class="form-inline">
+            <div class="col-lg-4">
+                <input type="text" class="form-control" name="cari"  style="width:370px" placeholder="Cari Pegawai berdasarkan nama atau alamat" value="{{ old('cari') }}">                </div>
+            <div class="col-sm-2">
+                <input type="submit" class="btn btn-default pull-right" value="CARI" >
+            </div>
+        </form>
+    </div>
+
+    <br>
+
 	<table class="table">
     <thead class="thead">
 		<tr>
 			<th>Nama</th>
-			<th>Jabatan</th>
-			<th>Umur</th>
 			<th>Alamat</th>
 			<th>Opsi</th>
 		</tr>
 		@foreach($pegawai as $p)
 		<tr>
 			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
 			<td>{{ $p->pegawai_alamat }}</td>
 			<td>
-
+                <a href="/pegawai/view/{{ $p->pegawai_id }}" class="label label-primary"> View Detail</a>
+				|
 				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="label label-info"> Edit</a>
 				|
 				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="label label-danger">Hapus</a>
-
             </td>
 		</tr>
 
@@ -38,7 +50,8 @@
      <thead>
 	</table>
 
-    <div class="d-flex text-center">
-        <a href="/pegawai/tambah" class="btn btn-primary"> Tambah Data</a>
+    <div class="text-center">
+        {{ $pegawai->links() }}
     </div>
+
 @endsection
