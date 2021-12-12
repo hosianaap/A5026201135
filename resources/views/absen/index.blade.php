5834777adf9568ab5276e29c857af2645b7d9c9a
hosianaarga <hosianaputri.205026@mhs.its.ac.id>
2021-12-12 19:08:50 +0700

    Update index.blade.php

--- resources/views/absen/index.blade.php
+++ resources/views/absen/index.blade.php
@@ -4,23 +4,29 @@
 <ul class="nav nav-pills nav-stacked">
     <li><a href="/pegawai" > Pegawai </a></li>
     <li class="active"><a href="/absen"> Absen </a></li>
-    <li> <a href="#"> Praktikum </a> </li>
+    <li> <a href="/beras"> Beras </a> </li>
 </ul>
 @endsection
 
 @section('judulbagian', 'Data Absen')
 @section('konten')
+    <div>
+       <a href="/absen/tambah" class="btn btn-primary"> + Tambah Data</a>
+    </div>
+
+    <br>
+
 	<table class="table">
         <thead class="thead">
 		<tr>
-			<th>IDPegawai</th>
+			<th>Nama Pegawai</th>
 			<th>Tanggal</th>
 			<th>Status</th>
 			<th>Opsi</th>
 		</tr>
 		@foreach($absen as $a)
 		<tr>
-			<td>{{ $a->IDPegawai }}</td>
+			<td>{{ $a->pegawai_nama }}</td>
 			<td>{{ $a->Tanggal }}</td>
 			<td>{{ $a->Status }}</td>
 			<td>
@@ -33,8 +39,15 @@
         </thead>
 	</table>
 
-    <div class="d-flex text-center">
-        <a href="/absen/tambah" class="btn btn-primary"> Tambah Data </a>
+    <div class="text-center">
+        {{ $absen->links()}}
     </div>
 
+    <p>
+        <strong> Keterangan Status </strong>
+        <br>
+        I : Izin <br>
+        S : Sakit <br>
+        A : Alpha <br>
+    </p>
 @endsection
